af60a2c0f2578c7ff34a211fd9d14094988e63a1
franz-josef-kaiser <wegithubmore@gmail.com>
2012-06-28 16:33:55 +0200

    added index.html and reverted prev version

--- plugin-dirs.php
+++ plugin-dirs.php
@@ -4,7 +4,7 @@
 Plugin Name:  Additional Plugin Directories
 Plugin URI:   http://github.com/chrisguitarguy
 Description:  A framework to allow adding additional plugin directories to WordPress
-Version:      0.7.1
+Version:      0.7.3
 Author:       Christopher Davis
 Contributors: Franz Josef Kaiser, Julien Chaumond
 Author URI:   http://christopherdavis.me
